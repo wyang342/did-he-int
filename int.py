259b5192c7fb9b6c514c8e5660201a32cfe3184e
Won Yang <won@gatech.edu>
2020-12-21 12:06:05 -0600

    can make requests now

--- int.py
+++ int.py
@@ -1,1 +1,45 @@
 import requests
+
+# Getting API key
+handle = open("api-key.txt", "r")
+api_key = handle.read()
+apiKeyQuestion = "?api_key=" + api_key
+apiKeyAnd = "&api_key=" + api_key
+
+# URLS
+baseUrl = "https://na1.api.riotgames.com"
+getSummonerByName = baseUrl + "/lol/summoner/v4/summoners/by-name/hyeonipus" + apiKeyQuestion
+
+# Getting accountID from summoner name
+responseFromSummonerName = requests.get(getSummonerByName)
+dataFromSummonerName = responseFromSummonerName.json()
+accountID = dataFromSummonerName['accountId']
+
+data = {"champions": {}, "queueType": {}, "roleType": {}}
+
+# Looping through to get all matches
+for startIndex in range(0, 501, 100):
+	beginIndex = "?beginIndex=" + str(startIndex)
+	# Getting MatchList from AccountID
+	getMatchList = baseUrl + "/lol/match/v4/matchlists/by-account/" + accountID + beginIndex + apiKeyAnd
+	responseFromMatchList = requests.get(getMatchList)
+	matchList = responseFromMatchList.json()
+	matches = matchList['matches']
+	for matchDict in matches:
+		champion = matchDict["champion"]
+		if not champion in data["champions"]:
+			data["champions"][champion] = 1
+		else:
+			data["champions"][champion] += 1
+		queue = matchDict["queue"]
+		if not queue in data["queueType"]:
+			data["queueType"][queue] = 1
+		else:
+			data["queueType"][queue] += 1
+		role = matchDict["role"]
+		if not role in data["roleType"]:
+			data["roleType"][role] = 1
+		else:
+			data["roleType"][role] += 1
+
+print(data)
